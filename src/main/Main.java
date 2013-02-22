0bc95ae9ecfe8ab447282003b34ac26091fac4cd
Jesper Plantener <jesper@plantener.dk>
2013-02-22 17:48:27 +0100

    Added function for thresholding image

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,9 @@
 package main;
+import com.googlecode.javacv.FrameGrabber.Exception;
 import com.googlecode.javacv.OpenCVFrameGrabber;
+import com.googlecode.javacv.cpp.*;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
-import static com.googlecode.javacv.cpp.opencv_highgui.*;
-import static com.googlecode.javacpp.Loader.*;
-import static com.googlecode.javacv.cpp.opencv_core.*;
-import static com.googlecode.javacv.cpp.opencv_imgproc.*;
-
+//import com.googlecode.javacpp.Loader.*;
 public class Main {
 
 	public static void main(String[] args) {
@@ -15,26 +13,42 @@ captureFrame();
 	}
 
 	private static void captureFrame() {
-		IplImage imgHSV = null;
 		IplImage img;
+		
 		
 		// 0-default camera, 1 - next...so on
 		final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-		try {
-			grabber.start();
-
-			img = grabber.grab();
-
-			cvCvtColor(img, imgHSV,CV_RGB2HSV);
-
-			if (img != null) {
-				cvSaveImage("capture.jpg", imgHSV);
-
+		
+			try {
+				grabber.start();
+				img = grabber.grab();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
 
+			//thresholdImage(img);
+
+	}
+	
+	public IplImage thresholdImage(IplImage img){
+		IplImage imgHSV;
+		IplImage imgThreshold;
+				
+		imgHSV = opencv_core.cvCreateImage(opencv_core.cvGetSize(img), 8, 3);
+		imgThreshold = opencv_core.cvCreateImage(opencv_core.cvGetSize(img), 8, 1);
+		
+		//Convert RGB image to HSV
+		opencv_imgproc.cvCvtColor(img, imgHSV,opencv_imgproc.CV_BGR2HSV);
+		
+		//Upper and lower bounds, notice that cvScalar is on this form: BGR, and not RGB
+		opencv_core.cvInRangeS(imgHSV, opencv_core.cvScalar(20, 100, 100,0),
+				opencv_core.cvScalar(30, 255, 255,0), imgThreshold);
+		
+		//used for testing
+		if (imgThreshold != null) {
+			opencv_highgui.cvSaveImage("capture.jpg", imgThreshold);
 		}
+		return imgThreshold;
 	}
 }
 
