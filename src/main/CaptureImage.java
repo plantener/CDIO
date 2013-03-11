9001d40316fa740d20aa0f2ca548b546c94b2e90
Jesper Plantener <jesper@plantener.dk>
2013-03-11 09:32:52 +0100

    new imageGrabber

--- src/main/CaptureImage.java
+++ src/main/CaptureImage.java
@@ -3,10 +3,14 @@ package main;
 import com.googlecode.javacv.OpenCVFrameGrabber;
 import com.googlecode.javacv.FrameGrabber.Exception;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_highgui;
+import com.googlecode.javacv.cpp.opencv_highgui.CvCapture;
 
 public class CaptureImage {
 	private final OpenCVFrameGrabber grabber;
 	private IplImage img = null;
+	
+	CvCapture capture = opencv_highgui.cvCreateCameraCapture(0);
 	
 	
 	public CaptureImage(){
@@ -23,20 +27,21 @@ public class CaptureImage {
 	
 	public IplImage grabImage(){
 		
-		
-		try {
-	
-		//A grabbed image from Logitech webcam is in following resolution: 1200x800px
-			
-			img = grabber.grab();
-			
-			
-			
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		}
+		img = opencv_highgui.cvQueryFrame(capture);
 		return img;
+//		try {
+//	
+//		//A grabbed image from Logitech webcam is in following resolution: 1200x800px
+//			
+//			img = grabber.grab();
+//			
+//			
+//			
+//		} catch (Exception e) {
+//			
+//			e.printStackTrace();
+//		}
+//		return img;
 	}
 	
 	
