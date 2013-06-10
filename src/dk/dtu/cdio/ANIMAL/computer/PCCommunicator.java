ba08c5eadcc20627ccf5ff8e7458e797716df9b8
Flemming Madsen <mail@leflings.dk>
2013-06-10 15:17:28 +0200

    Navigational updates

--- src/dk/dtu/cdio/ANIMAL/computer/PCCommunicator.java
+++ src/dk/dtu/cdio/ANIMAL/computer/PCCommunicator.java
@@ -3,6 +3,10 @@ package dk.dtu.cdio.ANIMAL.computer;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+import com.sun.jndi.url.corbaname.corbanameURLContextFactory;
 
 import lejos.pc.comm.NXTCommFactory;
 import lejos.pc.comm.NXTConnector;
@@ -14,11 +18,28 @@ public class PCCommunicator {
 	private NXTConnector connector;
 	private DataInputStream dataIn;
 	private DataOutputStream dataOut;
+	private Queue<Command> queue;
+	private Reader reader;
 
-	public PCCommunicator(NXTInfo nxtInfo) {
+	public PCCommunicator(NXTInfo nxtInfo, Queue<Command> queue) {
 		this.nxtInfo = nxtInfo;
+		this.queue = queue;
+		reader = new Reader();
 	}
-
+	
+	public void testLatency(){
+		long start = System.currentTimeMillis();
+		sendData(NavCommand.LATENCY_TEST.ordinal(), 0, 0, 0, false);
+		try {
+			dataIn.readInt();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(end-start);
+	}
+	
 	public void sendData(int code, float v0, float v1, float v2, boolean bit) {
 		try {
 			dataOut.writeInt(code);
@@ -50,11 +71,34 @@ public class PCCommunicator {
 			connected = false;
 			return connected;
 		}
-		// if (!reader.isRunning) {
-		// reader.start();
-		// }
+		if (!reader.isRunning) {
+			reader.start();
+		}
 		return connected;
 
+	}
+	
+	class Reader extends Thread {
+		
+		public boolean isRunning = false;
+		
+		public void run() {
+			int incoming;
+			isRunning = true;
+			while(isRunning) {
+				try {
+					incoming = dataIn.readInt();
+					if(NavCommand.POP.ordinal() == incoming) {
+						System.out.format("[Queue] size: %d, head: %s%n", queue.size()-1, queue.remove());
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					isRunning = false;
+				} catch (NoSuchElementException e) {
+					System.out.println("Queue empty, but pop received");
+				}
+			}
+		}
 	}
 
 }
